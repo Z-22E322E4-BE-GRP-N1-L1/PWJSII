45cbc710fa3092f07cda1cb387e3d79216e67f3e
Prof. Thiago Vieira de Aguiar <56936784+thiago-aguiar@users.noreply.github.com>
2022-10-12 01:12:35 +0000

    Aula 01: 11/10/22: Introdução sobre Arrays.

---
+++ Aula01_221011/script.js
@@ -0,0 +1,54 @@
+// Arrays
+// [ 'maçã', 'banana', 'bolo']
+// ['nome A', 7, 5, true]
+
+// let lista = new Array();
+// let lista = []; // new Array();
+//let lista = ['maça', 'banana', 'bolo'];
+
+// let lista = new Array('nome A', 7, 5, true);
+// let lista = new Array(10);
+// lista[99] = null;
+
+// console.log(typeof lista);
+// console.log(lista);
+
+//let alunoA = ['nome A', 7, 5, true];
+//console.log(alunoA);
+
+/* const lista = [];
+
+lista[0] = "Água";
+lista[10] = "Feijão"
+
+const item10 = lista[90];
+console.log(item10);
+
+lista = ['nome A', 7, 5, true]; */
+
+// const alunos = ['Aluna A', "Aluna B", "Aluna C"];
+const alunos = [];
+// console.log(alunos);
+
+alunos[0] = 9.8;
+alunos[1] = 7.7;
+alunos[2] = 4.5;
+alunos[3] = 7.9;
+alunos[4] = 7.9;
+alunos[5] = 7.9;
+alunos[6] = 10.9;
+
+// delete alunos[2];
+alunos[2] = null;
+const lastIndex = alunos.length - 1;
+const lastAluno = lastIndex; // alunos[lastIndex];
+console.log(lastAluno);
+
+alunos[-1] = -10.0;
+console.log(alunos[-1]);
+console.log(alunos[0]);
+// alunos = [9.8, 7.7, 4.5];
+
+
+// lista[0] = "Feijão";
+// console.log(lista);
